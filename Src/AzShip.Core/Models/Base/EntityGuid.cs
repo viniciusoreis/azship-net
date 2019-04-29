7e9cc24c329cb8e79d5a9d3181932cfd376e285d
Vinicius Reis <vinicius_reis_df@hotmail.com>
2019-04-29 09:59:32 -0300

    init files

---
+++ Src/AzShip.Core/Models/Base/EntityGuid.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AzShip.Core.Models.Base
+{
+    public abstract class EntityGuid
+    {
+
+        public Guid Id { get; protected set; }
+
+        public override bool Equals(object obj)
+        {
+            var compareTo = obj as EntityGuid;
+
+            if (ReferenceEquals(this, compareTo)) return true;
+            if (ReferenceEquals(null, compareTo)) return false;
+
+            return Id.Equals(compareTo.Id);
+        }
+
+        public static bool operator ==(EntityGuid a, EntityGuid b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+                return true;
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(EntityGuid a, EntityGuid b)
+        {
+            return !(a == b);
+        }
+
+        public override int GetHashCode()
+        {
+            return (GetType().GetHashCode() * 907) + Id.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return GetType().Name + " [Id=" + Id + "]";
+        }
+
+        public void SetId(Guid id)
+        {
+            this.Id = id;
+        }
+    }
+}
---
+++ Src/AzShip.Core/Models/Cargo.cs
@@ -0,0 +1,17 @@
+﻿using AzShip.Core.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AzShip.Core.Models
+{
+    public class Cargo : EntityGuid
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string SourceAddress { get; set; }
+        public string DestinationAddress { get; set; }
+        public string ContactEmail { get; set; }
+    }
+}
